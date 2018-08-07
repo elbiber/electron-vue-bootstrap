fd8b453a3bf0d1aca6d93bd5bd91b270f878e393
Willy Weise <hellmuth.willy.weise@stud.fh-luebeck.de>
2018-08-07 13:31:49 +0200

    vue-part-simple stable

--- src/renderer/app.js
+++ src/renderer/app.js
@@ -1,5 +1,17 @@
-document.addEventListener( "DOMContentLoaded", () => {
-  const version = process.version;
-  const e = document.getElementById( "info" );
-  e.textContent = `I'm running Node.js version: ${ version }`;
-} );
+const axios = require('axios');
+const api = require('./api.json');
+
+new Vue({
+    el: '#app',
+    data: {      
+        info: null,
+        stockName: null    
+    },
+    methods: {
+      formSubmitted: function(){
+          axios
+          .get('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=Acc&interval=1min&apikey='+api.key)
+          .then(response => (this.info = response))
+      }
+    }
+  });
