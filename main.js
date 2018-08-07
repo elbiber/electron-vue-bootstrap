e9b6d870bf5f2b338cec98990cc394ba0bc28b2e
Willy Weise <hellmuth.willy.weise@stud.fh-luebeck.de>
2018-08-07 13:43:47 +0200

    adjusted size

--- main.js
+++ main.js
@@ -1,56 +1,56 @@
-'use strict';
+"use strict";
 
 const electron = require("electron");
 const path = require("path");
 const reload = require("electron-reload");
 const isDev = require("electron-is-dev");
 const { app, BrowserWindow, ipcMain, dialog } = electron;
-const menus = require( "./src/main/menus" );
+const menus = require("./src/main/menus");
 
-let mainWindow = null
+let mainWindow = null;
 
 if (isDev) {
-  reload(__dirname, {electron: require("electron-prebuilt")});
+  reload(__dirname, { electron: require("electron-prebuilt") });
 }
 
-app.on( "window-all-closed", () => {
-    if ( process.platform !== "darwin" ) {
-        app.quit();        
-    }
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
 
-app.on( "ready", () => {
-    mainWindow = new BrowserWindow( { width: 600, height: 600, show: false } );
-    mainWindow.loadURL( `file://${ __dirname }/index.html` );
-    if ( isDev ) {
-        mainWindow.webContents.openDevTools();
-        //require('vue-devtools').install();
-    }
-    mainWindow.once( "ready-to-show", () => {
-        mainWindow.show();
-    });
-    mainWindow.on( "closed", () => {
-        mainWindow = null;
-    });
-    menus.buildMenu();
+app.on("ready", () => {
+  mainWindow = new BrowserWindow({ width: 1920, height: 1080, show: false });
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+    //require('vue-devtools').install();
+  }
+  mainWindow.once("ready-to-show", () => {
+    mainWindow.show();
+  });
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+  menus.buildMenu();
 });
 
-ipcMain.on( "show-dialog", ( e, arg ) => {
-    const msgInfo = {
-        title: "My App Alert",
-        message: arg.message,
-        buttons: [ "OK" ]
-    };
-    dialog.showMessageBox( msgInfo );
-    e.sender.send('click-reply', 'clicking back');
+ipcMain.on("show-dialog", (e, arg) => {
+  const msgInfo = {
+    title: "My App Alert",
+    message: arg.message,
+    buttons: ["OK"]
+  };
+  dialog.showMessageBox(msgInfo);
+  e.sender.send("click-reply", "clicking back");
 });
 
-ipcMain.on('asynchronous-message', (event, arg) => {
-    console.log(arg) // prints "ping"
-    event.sender.send('asynchronous-reply', 'pong')
+ipcMain.on("asynchronous-message", (event, arg) => {
+  console.log(arg); // prints "ping"
+  event.sender.send("asynchronous-reply", "pong");
 });
-  
- ipcMain.on('synchronous-message', (event, arg) => {
-    console.log(arg) // prints "ping"
-    event.returnValue = 'pong'
- });
+
+ipcMain.on("synchronous-message", (event, arg) => {
+  console.log(arg); // prints "ping"
+  event.returnValue = "pong";
+});
